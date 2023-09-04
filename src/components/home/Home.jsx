9eac1d8a6fa385e3c772f73d6d92b4d407e30263
VaniaML <107821662+VaniaML@users.noreply.github.com>
2023-09-04 15:24:40 -0600

    Add files via upload

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -18,39 +18,46 @@ export default function Home() {
         <div className={style.Line1}>
         <hr className={style.Line}></hr>
         </div>
-        <div className={style.Line1}>
-        <img className={style.Imgs} src={ Nieve } alt="Nv" />        
-        <img  className={style.Imgs} src={ Lap } alt="Lp" />
-        <img className={style.Imgs} src={ Patin } alt="Pt" />
-        <img className={style.Imgs} src={ Cerrucho } alt="Ch" />
-        <img className={style.Imgs} src={ Carro } alt="Cr" />
-        <img className={style.Imgs} src={ Carreta } alt="Ct" />
-        </div>
+        
+        <div className={style.LineContainer}>
+        <div className={style.ImgContainer}>     
+          <img className={style.Imgs} src={ Nieve } alt="Nv" />
+          <div className= {style.Name}>Squirrels</div>
+          <div className= {style.Text}>We are building a snowman</div>
+          </div>
 
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      <p>HOLA</p>
-      </div>
-      
+          <div className={style.ImgContainer}>
+          <img  className={style.Imgs} src={ Lap } alt="Lp" />
+          <div className= {style.Name}>Squirrel</div>
+          <div className= {style.Text}>Working with my laptop</div>
+          </div>
+
+          <div className={style.ImgContainer}>
+          <img className={style.Imgs} src={ Patin } alt="Pt" />
+          <div className= {style.Name}>Squirrel</div>
+          <div className= {style.Text}>I work polishing the floor</div>
+          </div>
+          </div>
+
+          <div className={style.LineContainer}>
+          <div className={style.ImgContainer}>
+          <img className={style.Imgs} src={ Cerrucho } alt="Ch" />
+          <div className= {style.Name}>Squirrel</div>
+          <div className= {style.Text}>Working cutting snowballs</div>
+          </div>
+
+          <div className={style.ImgContainer}>
+          <img className={style.Imgs} src={ Carro } alt="Cr" />
+          <div className= {style.Name}>Squirrel</div>
+          <div className= {style.Text}>Driving on my way to work</div>
+          </div>
+
+          <div className={style.ImgContainer}>
+          <img className={style.Imgs} src={ Carreta } alt="Ct" />
+          <div className= {style.Name}>Squirrel</div>
+          <div className= {style.Text}>Carrying wood for the campfire</div>
+          </div>
+          </div>
+          </div>
+    
       )}
