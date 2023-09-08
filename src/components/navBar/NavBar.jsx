b8ba67a8d25e9e8fae47f2994e1632c34bbb76ec
VaniaML <107821662+VaniaML@users.noreply.github.com>
2023-09-07 18:08:40 -0600

    Add files via upload

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -7,7 +7,7 @@ return(
   <div>
     <div className={style.NavBar}>
       <h1 className={style.Text}>Hardworking Squirrel</h1>
-      <button className={style.Btn}>HOME</button>	
+      <button className={style.Btn}>HOME</button>
       <button className={style.Btn}>SERVICES</button>
       <button className={style.Btn1}>ABOUT</button>
       <button className={style.Btn1}>CONTACT</button>
