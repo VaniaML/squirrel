2c42713c22ce8784f10c9484347799473e3548d3
VaniaML <107821662+VaniaML@users.noreply.github.com>
2023-09-13 16:59:35 -0600

    Add files via upload

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,10 +10,10 @@ export default function Footer() {
         <h1 className={style.Text}>Hardworking Squirrel</h1>
         <p className={style.Name}>Vania Martinez L.</p>
         <div className={style.Btns}>
-        <button className={style.Btn}>HOME</button>	
-        <button className={style.Btn}>SERVICES</button>
-        <button className={style.Btn1}>ABOUT</button>
-        <button className={style.Btn1}>CONTACT</button>
+        <a href='#Home' className={style.Btn}>HOME</a>
+        <a href='#Services' className={style.Btn}>SERVICES</a>
+        <a href='#About' className={style.Btn1}>ABOUT</a>
+        <a href='#Contact' className={style.Btn1}>CONTACT</a>
         </div>
         </div>
         </div>
